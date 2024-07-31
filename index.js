03703b3309a06d772a7f9018897b98ef512467d2
Maksym P. <maxim.panchenko93@gmail.com>
2024-07-31 03:31:32 +0300

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
+//document.querySelector("button").addEventListener("click", handleClick);
+
+// function handleClick() {
+//   alert("I got clicked!");
+// }
+
+// document.getElementById("speedText").addEventListener("input", function () {
+//   var speedValue = prompt(this.addEventListener, "");
+// });
+
+// function returnText() {
+//   let speedInput = document.getElementById("speedNumber").value;
+//   let percentInput = document.getElementById("percentNumber").value;
+
+//   alert(speedInput);
+//   alert(percentInput);
+// };
+
 let speedValue = 0;
 let percentValue = 0;
 
@@ -16,6 +34,7 @@ function updatePercentValue(event) {
 function displayResult(event) {
   let resultValue = (speedValue * percentValue) / 100 + speedValue;
   document.getElementById("resultNum").value = resultValue.toFixed(2);
+
   console.log("Result Value:", resultValue);
 }
 
@@ -31,6 +50,8 @@ function valuesReset() {
   console.log("All values reset.");
 }
 
+// Event listeners
+
 document
   .getElementById("speedNumber")
   .addEventListener("input", updateSpeedValue);
@@ -43,3 +64,12 @@ document
   .addEventListener("click", displayResult);
 
 document.getElementById("reset").addEventListener("click", valuesReset);
+
+document
+  .getElementById("calcForm")
+  .addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      event.preventDefault(); // Prevent the default form submission behavior
+      displayResult(); // Call the function directly or use button click
+    }
+  });
